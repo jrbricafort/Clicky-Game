ad00f7a5d3c5f18127b840181497f01dcdfc5fcc
jrbricafort <josephricafort1@gmail.com>
2019-06-23 00:02:47 -0400

    package something

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import Card from "./components/card";
-import Wrapper from "./components/wrapper";
-import Header from "./components/header";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 import cards from "./cards.json";
-// import "./App.css";
+import "./App.css";
 
 class App extends Component {
     // Setting this.state.cards to the cards json array
