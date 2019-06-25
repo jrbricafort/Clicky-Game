3476ce4f197e6ef7917aa74d4d41a22e171b519a
jrbricafort <josephricafort1@gmail.com>
2019-06-25 18:36:10 -0400

    working app

---
+++ build/precache-manifest.24480650f030e5719cff5fe8c9887a49.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627f0cfd2def9959e5f9c55ba4700ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4aca38e3d2ec2652c084",
+    "url": "/static/js/main.11c24361.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
